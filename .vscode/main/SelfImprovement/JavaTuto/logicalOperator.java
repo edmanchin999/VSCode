9f97a563b3563184d04949a9f782fb7cc58690f4
Edman Chin <eddie.chin999@hotmail.com>
2022-08-19 09:38:54 +0800

    Master

---
+++ .vscode/main/SelfImprovement/JavaTuto/logicalOperator.java
@@ -0,0 +1,13 @@
+package SelfImprovement.JavaTuto;
+
+import java.util.*;
+
+public class logicalOperator {
+    public static void main(String[] args) {
+        // Logical Operator = combination of 2 different expression
+        // && = AND operator
+        // || = OR operator
+        // ! = NOT operator
+
+    }
+}
